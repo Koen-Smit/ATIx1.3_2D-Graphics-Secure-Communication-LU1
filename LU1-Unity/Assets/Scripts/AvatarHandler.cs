ddf94e66c9d2ba113dea74434c881a37c2517681
ZeeSterre <sterre17032005@gmail.com>
2025-03-28 11:25:51 +0100

    Gegevens overzicht toegevoegd
    Gegevens overzicht toegevoegd aan het instellingen panel bij beide routes: terugvinden van gegevens/eventueel avatar

--- LU1-Unity/Assets/Scripts/AvatarHandler.cs
+++ LU1-Unity/Assets/Scripts/AvatarHandler.cs
@@ -7,43 +7,43 @@ public class AvatarHandler : MonoBehaviour
 
     public Image[] colorPanels;
 
-    private Color defaultColor = new Color(162f / 255f, 228f / 255f, 184f / 255f); // Groen
+    private Color defaultColor = new Color(162f / 255f, 228f / 255f, 184f / 255f); 
 
-    // Kleur voor de geselecteerde avatar
-    private Color selectedColor = new Color(184f / 255f, 255f / 255f, 255f / 255f); // Wit (de geselecteerde kleur)
+    
+    private Color selectedColor = new Color(184f / 255f, 255f / 255f, 255f / 255f); 
 
     void Start()
     {
-        // Zorg ervoor dat alle vakjes de groene kleur hebben bij het starten van het spel
+        
         ResetAvatarColors();
 
-        // Voeg de methodes toe aan de knoppen zodat ze de SelectAvatar methode aanroepen
+        
         for (int i = 0; i < avatarButtons.Length; i++)
         {
-            int index = i;  // Local variable for lambda expression to capture the correct index
+            int index = i; 
             avatarButtons[i].onClick.AddListener(() => SelectAvatar(index));
         }
     }
 
-    // Methode om de kleur te veranderen wanneer een avatar wordt geselecteerd
+    
     public void SelectAvatar(int avatarIndex)
     {
         // Verander de kleur van het gekleurde vakje van de geselecteerde avatar
         for (int i = 0; i < colorPanels.Length; i++)
         {
             if (i == avatarIndex)
-                colorPanels[i].color = selectedColor;  // Stel de geselecteerde kleur in (bijvoorbeeld wit)
+                colorPanels[i].color = selectedColor;  
             else
-                colorPanels[i].color = defaultColor;  // Zet de andere vakjes terug naar groen
+                colorPanels[i].color = defaultColor;  
         }
     }
 
-    // Reset alle kleuren naar de groene kleur (beginkleur)
+    
     private void ResetAvatarColors()
     {
         for (int i = 0; i < colorPanels.Length; i++)
         {
-            colorPanels[i].color = defaultColor;  // Zet alle vakjes op groen bij het starten
+            colorPanels[i].color = defaultColor; 
         }
     }
 }
