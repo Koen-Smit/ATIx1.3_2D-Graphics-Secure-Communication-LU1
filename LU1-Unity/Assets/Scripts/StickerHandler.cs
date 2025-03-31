18ad624c8386a36a1d71e8b0840935be7a773d1a
KerrieMeister96 <koensmit13@gmail.com>
2025-04-01 00:00:24 +0200

    last module fix

--- LU1-Unity/Assets/Scripts/StickerHandler.cs
+++ LU1-Unity/Assets/Scripts/StickerHandler.cs
@@ -59,9 +59,10 @@ public class StickerHandler : MonoBehaviour
             {
                 case 10: return 0;
                 case 11: return 1;
-                case 7: return 2;
-                case 8: return 3;
-                case 9: return 4;
+                case 6: return 2;
+                case 7: return 3;
+                case 8: return 4;
+                case 9: return 5;
                 default: return -1;
             }
         }
